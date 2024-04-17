c65a5a8b83cd63e68487021b149e4817447cf5a3
unknown <ajibadeifedolapo17@gmail.com>
2024-04-17 18:56:04 +0100

    Pushing final changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -61,21 +61,22 @@ export default function Home() {
         await window.ethereum.request({
           method: "wallet_addEthereumChain",
           params: [{
-            chainId: "0x61",
-            chainName: "Binance Smart Chain Testnet",
+            chainId: "0x38", // Chain ID for Binance Smart Chain Mainnet
+            chainName: "Binance Smart Chain Mainnet",
             nativeCurrency: {
               name: "BNB",
-              symbol: "tBNB",
+              symbol: "BNB",
               decimals: 18,
             },
-            rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545"],
-            blockExplorerUrls: ["https://testnet.bscscan.com"],
+            rpcUrls: ["https://bsc-dataseed.binance.org/"],
+            blockExplorerUrls: ["https://bscscan.com"],
           }],
         });
         
         
+        
         // Switch to the preferred network
-        await Moralis.switchNetwork("0x61");
+        await Moralis.switchNetwork("0x38");
         console.log(`Switched to preferred network`);
       } else {
         console.log("Already connected to the preferred network");
@@ -87,14 +88,19 @@ export default function Home() {
   };
 
   return (
-      <div>
-    <div className="flex flex-row bg-[#07091a] items-center text-white justify-between px-2 md:px-10 py-2">
-      <img src={Bits} />
+    <div>
       {account ? (
-        <div className="ml-5 md:ml-0 px-2 md:px-4 py-2 bg-[#d55025] text-white text-center rounded-xl border-2 border-slate-300">
+        <div className="flex flex-row bg-[#07091a] items-center text-white justify-between px-2 md:px-10 py-2">
+        <img src={Bits} />
+        <div  className="ml-5 md:ml-0 px-2 md:px-4 py-2 bg-[#d55025] text-white text-center rounded-xl border-2 border-slate-300">
+          
           Connected to {account.slice(0, 6)}...{account.slice(account.length - 4)}
         </div>
+        </div>
       ) : (
+        <div className="flex flex-col ">
+        <div className="flex flex-row bg-[#07091a] items-center text-white justify-between px-2 md:px-10 py-2 mb-24">
+        <img src={Bits} />
         <button
         className="px-4 py-2 bg-[#d55025] text-white rounded-xl border-2 border-slate-300"
           onClick={handleConnectClick}
@@ -102,10 +108,17 @@ export default function Home() {
         >
           {isConnecting ? "Connecting..." : "Connect"} {/* Show "Connecting..." text while connecting */}
         </button>
+        </div>
+        <div class="flex items-center justify-center">
+  <h1 class="text-center text-2xl lg:text-4xl font-normal lg:font-bold text-white leading-10">
+    Welcome to <span className="text-[#d55025] animate-pulse duration-700">BitsLab.</span> Kindly connect your wallet.
+  </h1>
+</div>
+
+        </div>
       )}
-  </div>
       {showMessage ? (
-        chainId === "0x61" ? (
+        chainId === "0x38" ? (
           <div className="flex md:flex-wrap flex-col justify-center w-screen md:flex-row gap-6 h-full text-white py-2 md:px-10 md:py-5">
             <Stage />
             <Price />
@@ -119,12 +132,11 @@ export default function Home() {
             <h1 className="text-center md:w-1/2 w-full text-white md:text-2xl text-xl mb-4">Please click the button below to switch to the preferred network</h1>
               <button className="ml-5 md:ml-0 px-2 md:px-4 py-2 bg-[#d55025] text-white text-center rounded-xl border-2 border-slate-300" onClick={switchToPreferredNetwork}>Switch Network</button>
           </div>
-          
         )
       ) : null}
-    
-    </div>
+        </div>
   );
+
 }
 
 
--- src/constants.js
+++ src/constants.js
@@ -3,10 +3,10 @@ import Presale from './ABI/Presale.json';
 
 
 
-export const BEP20Token_Address = "0xb432a89eCFeE07137f2C69EE3BeC43db3f11bbDF";
+export const BEP20Token_Address = "0xAb80553A488CCC51e984b14b6BEB6576bd91C9D9";
 export const BEP20Token_ABI = BEP20Token;
 
-export const Presale_Address = {"97": ["0xd744De4af27A51c9dB46aA2932Aabb844789431c"]};// Testnet - Replace with actual testnet address // Mainnet
+export const Presale_Address = {"56": ["0x161a9DBA3952431715AbF42FAD76445278372F43"]};// Testnet - Replace with actual testnet address // Mainnet
     //56: "" // Testnet - Replace with actual testnet address
  //56: "" // Testnet - Replace with actual testnet address
 export const Presale_ABI = Presale;
@@ -14,4 +14,3 @@ export const Presale_ABI = Presale;
 export const address = "0xd744De4af27A51c9dB46aA2932Aabb844789431c";
 
 export const abi = Presale_ABI;
-  
