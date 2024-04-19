6b39d490acb0e177ed961b7069c2c35166487747
unknown <ajibadeifedolapo17@gmail.com>
2024-04-19 17:37:03 +0100

    Final Changes

--- src/constants.js
+++ src/constants.js
@@ -6,11 +6,11 @@ import Presale from './ABI/Presale.json';
 export const BEP20Token_Address = "0xAb80553A488CCC51e984b14b6BEB6576bd91C9D9";
 export const BEP20Token_ABI = BEP20Token;
 
-export const Presale_Address = {"56": ["0x161a9DBA3952431715AbF42FAD76445278372F43"]};// Testnet - Replace with actual testnet address // Mainnet
+export const Presale_Address = {"56": ["0x883f991aa3af4f49c950cf742a77d169c8f5391b"]};// Testnet - Replace with actual testnet address // Mainnet
     //56: "" // Testnet - Replace with actual testnet address
  //56: "" // Testnet - Replace with actual testnet address
 export const Presale_ABI = Presale;
 
-export const address = "0xd744De4af27A51c9dB46aA2932Aabb844789431c";
+export const address = "0x883f991aa3af4f49c950cf742a77d169c8f5391b";
 
 export const abi = Presale_ABI;
